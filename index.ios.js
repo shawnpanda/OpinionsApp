04cf91896a83d1f3dd15c2672b2ef4e09360f2ea
Shawn Pan <xpan1@swarthmore.edu>
2016-09-17 14:27:56 -0400

    project init

---
+++ index.ios.js
@@ -0,0 +1,10 @@
+/**
+ * Opinion App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import { AppRegistry } from 'react-native';
+import OpinionsApp from './app/App'
+
+AppRegistry.registerComponent('OpinionsApp', () => OpinionsApp);
