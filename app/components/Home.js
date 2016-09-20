2e9c0db22b775a4304bf38ed854b9bd4a73db22b
Shawn Pan <xpan1@swarthmore.edu>
2016-09-20 19:14:47 -0400

    two bg images render correctly in home

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,26 +1,50 @@
 import React, { Component, PropTypes} from 'react'
-import { View, Text, ListView, StyleSheet, TouchableOpacity } from 'react-native'
+import { View, Text, ListView, StyleSheet, TouchableOpacity, Image } from 'react-native'
 import Swiper from 'react-native-swiper';
 
 
 class Home extends Component {
-  renderTopic(topic) {
+  renderSlide(topic, i) {
     return (
-      <TouchableOpacity>
-        <Text>abc</Text>
-      </TouchableOpacity>
+      <View style={styles.slide} key={i}>
+        <Image
+          style={styles.image}
+          source={{uri: 'https://avatars2.githubusercontent.com/u/9221501?v=3&s=466'}}
+          />
+        <Text>{topic.title}</Text>
+        <Text style={styles.text}>Hello Swiper</Text>
+      </View>
     )
   }
   render() {
+    let topics = this.props.topics;
     return (
       <Swiper style={styles.wrapper} showsButtons={true}>
-        <View style={styles.slide1}>
-          <Text style={styles.text}>Hello Swiper</Text>
-        </View>
-        <View style={styles.slide2}>
+        
+        <View style={styles.slide}>
+          <View style={styles.container}>
+            <Image
+              style={styles.image}
+              source={{uri: 'https://avatars2.githubusercontent.com/u/9221501?v=3&s=466'}}
+              />
+              <View style={styles.backdropView}>
+                <Text style={styles.text}>Hello Swiper</Text>
+              </View>
+          </View>
+          <View style={styles.container}>
+            <Image
+              style={styles.image}
+              source={{uri: 'https://avatars2.githubusercontent.com/u/9221501?v=3&s=466'}}
+              />
+              <View style={styles.backdropView}>
+                <Text style={styles.text}>Hello Swiper2</Text>
+              </View>
+          </View>
+      </View>
+        <View style={styles.slide}>
           <Text style={styles.text}>Beautiful</Text>
         </View>
-        <View style={styles.slide3}>
+        <View style={styles.slide}>
           <Text style={styles.text}>And simple</Text>
         </View>
       </Swiper>
@@ -31,28 +55,28 @@ class Home extends Component {
 const styles = StyleSheet.create({
   wrapper: {
   },
-  slide1: {
+  slide: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#9DD6EB',
-  },
-  slide2: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#97CAE5',
-  },
-  slide3: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#92BBD9',
+    backgroundColor: 'transparent',
+    backgroundColor: '#ddd',
   },
   text: {
-    color: '#fff',
+    color: '#000',
     fontSize: 30,
     fontWeight: 'bold',
+  },
+  image: {
+    flex: 1,
+    alignSelf: 'stretch'
+  },
+  container: {
+    flex: 2
+  },
+  backdropView: {
+    position: 'absolute',
+    left: 0,
+    top:0
   }
 })
 
--- app/reducers/reducers.js
+++ app/reducers/reducers.js
@@ -1,9 +1,29 @@
+'use strict'
 import { combineReducers } from 'redux'
 import commentReducer from './commentReducers'
+import { Map, Record, List, fromJS } from 'immutable'
+
+const topicInitialState = List([
+  Record({
+    title: "Gun Control",
+    article1Title: "Pro Gun Control",
+    article1Link: "http://www.latimes.com/opinion/op-ed/la-oe-defilippis-hughes-gun-myths-debunked-20160708-snap-story.html",
+    article2Title: "Against Gun Control",
+    article2: "http://www.baltimoresun.com/news/opinion/editorial/bs-ed-guns-checks-20140219-story.html",
+  })
+])
+
+function topicReducer(state = topicInitialState, action) {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
 
 
 const rootReducer = combineReducers({
-  comment: commentReducer
+  comment: commentReducer,
+  topics: topicReducer
 })
 
 export default rootReducer
